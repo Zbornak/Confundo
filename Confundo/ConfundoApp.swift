563284adf717921ee31dcc0690126a9277c5ca8b
Mark <zbornack@protonmail.com>
2023-02-21 15:29:38 +0000

    Initial Commit

---
+++ Confundo/ConfundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfundoApp.swift
+//  Confundo
+//
+//  Created by Mark Strijdom on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConfundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
