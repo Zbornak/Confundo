1e67fe71801c2ca1f96b0f29aeb9c5c20f0cdb7a
Mark <zbornack@protonmail.com>
2023-02-21 16:14:56 +0000

    Began basic styling, game engine and added all assets

--- Confundo/ContentView.swift
+++ Confundo/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let imageSet = Set(["bear", "buffalo", "chick", "chicken", "cow", "crocodile", "dog", "duck", "elephant", "frog", "giraffe", "goat", "gorilla", "hippo", "horse", "monkey", "moose", "narwhal", "owl", "panda", "parrot", "penguin", "pig", "rabbit", "rhino", "sloth", "snake", "walrus", "whale", "zebra"])
+    
+    @State private var userGuess = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Text("Confundo!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Image(imageSet.randomElement() ?? "dog")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: geometry.size.width * 0.8)
+                    .shadow(radius: 10)
+                    .padding()
+                    
+                Text("Guess the image as soon as you can!")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+                TextField("Enter your guess here", text: $userGuess)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
