0d0af2933e0037b5937c54529447c735bed8f970
Mark <zbornack@protonmail.com>
2023-02-21 16:24:56 +0000

    Added linear gradient, blurAmount variable

--- Confundo/ContentView.swift
+++ Confundo/ContentView.swift
@@ -12,45 +12,52 @@ struct ContentView: View {
     
     @State private var userGuess = ""
     
+    @State private var blurAmount = 0.0
+    
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-                HStack {
-                    Image(systemName: "eyes")
-                        .font(.largeTitle)
-                    Text("Confundo!")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                }
-                .padding(.bottom)
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .bottomLeading, endPoint: .topLeading)
+                    .ignoresSafeArea()
                 
-                Image(imageSet.randomElement() ?? "dog")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: geometry.size.width * 0.8)
-                    .shadow(radius: 10)
-                    .padding()
+                VStack {
+                    HStack {
+                        Image(systemName: "eyes")
+                            .font(.largeTitle)
+                        Text("Confundo!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                    }
+                    .padding(.bottom)
                     
-                Text("Guess the image as soon as you can!")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                
-                TextField("Enter your guess here", text: $userGuess)
-                    .textFieldStyle(.roundedBorder)
-                
-                Spacer()
-                
-                HStack {
-                    Text("Time remaining:")
+                    Image(imageSet.randomElement() ?? "dog")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.8)
+                        .shadow(radius: 10)
+                        .padding()
+                    
+                    Text("Guess the image as soon as you can!")
                         .font(.title3)
                         .fontWeight(.bold)
+                    
+                    TextField("Enter your guess here", text: $userGuess)
+                        .textFieldStyle(.roundedBorder)
+                    
                     Spacer()
-                    Text("Score:")
+                    
+                    HStack {
+                        Text("Time remaining:")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("Score:")
+                    }
+                    .font(.title3)
+                    .fontWeight(.bold)
                 }
-                .font(.title3)
-                .fontWeight(.bold)
+                .padding()
             }
-            .padding()
         }
     }
 }
